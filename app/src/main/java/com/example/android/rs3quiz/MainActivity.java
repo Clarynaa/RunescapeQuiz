ad79ac8b483182a362e1753a4033c7196cd77fa7
Clarynaa <Clary.v.green@gmail.com>
2018-04-30 17:50:35 -0500

    V1.0

---
+++ app/src/main/java/com/example/android/rs3quiz/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.android.rs3quiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    int totalQuestions = 4;
+    public String godsname = "guthix";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void submitAnswers(View view) {
+        int correctAnswers = 0;
+        RadioButton answerOne = findViewById(R.id.j42001);
+        if (answerOne.isChecked()) {
+            correctAnswers++;
+            Log.v("MainActivity", "year");
+        }
+        CheckBox zilyana = findViewById(R.id.zilyana);
+        CheckBox bandos = findViewById(R.id.Bandos);
+        CheckBox kree = findViewById(R.id.Kree);
+        CheckBox saradomin = findViewById(R.id.Saradomin);
+        CheckBox zaros = findViewById(R.id.Zaros);
+        CheckBox mah = findViewById(R.id.Mah);
+        CheckBox guthix = findViewById(R.id.Guthix);
+        CheckBox nex = findViewById(R.id.Nex);
+        if ((bandos.isChecked() && saradomin.isChecked() && zaros.isChecked() && mah.isChecked() && guthix.isChecked()) && !zilyana.isChecked() && !kree.isChecked() && !nex.isChecked()) {
+            correctAnswers++;
+            Log.v("MainActivity", "gods");
+        }
+        else if (!mah.isChecked()){
+            Toast.makeText(MainActivity.this, getString(R.string.mah), Toast.LENGTH_SHORT).show();
+        }
+        EditText godname = findViewById(R.id.godname);
+        String godtext = godname.getText().toString().toLowerCase();
+        Log.v("Main", "you typed: " + godtext.toLowerCase());
+        if (godtext.equals("guthix")) {
+            correctAnswers++;
+            Log.v("MainActivity", "Guthix");
+        }
+        RadioButton godsword = findViewById(R.id.gs5);
+        if (godsword.isChecked()) {
+            correctAnswers++;
+            Log.v("MainActivity", "Gs");
+        }
+        Toast.makeText(MainActivity.this, getString(R.string.yourscore ) + " " + correctAnswers + " out of " + totalQuestions, Toast.LENGTH_LONG).show();
+
+        //String yourScore = "Your score is: " + correctAnswers + " out of " + totalQuestions;
+        Button score = findViewById(R.id.score);
+        score.setText("Resubmit Answers");
+    }
+}
